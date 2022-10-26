a86085d5767b8a9304eb7a7982ec8f9375c9f981
Phachara Chirapakachote <46133542+ModzabazeR@users.noreply.github.com>
2022-10-26 12:39:31 +0700

    C00 [2nd try 70%]

---
+++ c00/c00_2ndtry/ex08/ft_print_combn.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_combn.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: phchirap <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/10 14:35:12 by phchirap          #+#    #+#             */
+/*   Updated: 2022/10/12 15:53:08 by phchirap         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_range(int range[], int n)
+{
+	int	i;
+
+	i = 0;
+	while (++i < n)
+	{
+		if (range[i - 1] >= range[i])
+		{
+			return ;
+		}
+	}
+	i = -1;
+	while (++i < n)
+	{
+		ft_putchar(range[i] + 48);
+	}
+	if (range[0] < 10 - n)
+	{
+		ft_putchar(',');
+		ft_putchar(' ');
+	}
+}
+
+void	ft_print_combn(int n)
+{
+	int	i;
+	int	range[10];
+
+	i = -1;
+	while (++i < n)
+	{
+		range[i] = i;
+	}
+	while (range[0] <= 10 - n)
+	{
+		ft_print_range(range, n);
+		range[n - 1]++;
+		i = n;
+		while (i)
+		{
+			i--;
+			if (range[i] > 9)
+			{
+				range[i - 1]++;
+				range[i] = 0;
+			}
+		}
+	}
+}
