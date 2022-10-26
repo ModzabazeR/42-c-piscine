2d30fcbace6a73616cb98fffd648b9260a81b1b9
Phachara Chirapakachote <46133542+ModzabazeR@users.noreply.github.com>
2022-10-26 12:49:08 +0700

    C04 [1st try 85%]

---
+++ c04/ex02/ft_putnbr.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: phchirap <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/06 12:15:45 by phchirap          #+#    #+#             */
+/*   Updated: 2022/10/19 16:57:37 by phchirap         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		return ;
+	}
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+	}
+	if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+	{
+		ft_putchar(nb + 48);
+	}
+}
+/*
+#include <stdlib.h>
+int main(int ac, char **av) {
+	ft_putnbr(atoi(av[1]));
+}*/
